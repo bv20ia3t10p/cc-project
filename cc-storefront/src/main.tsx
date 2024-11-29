0d4ffac63b0d4a126cef88adcdf90ecfd317a8fe
Phuc <beaver20310@gmail.com>
2024-11-29 18:59:56 +0700

    2

--- cc-storefront/src/main.tsx
+++ cc-storefront/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';  // Make sure your App component is correctly imported
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
--- cc-storefront/src/routes.tsx
+++ cc-storefront/src/routes.tsx
@@ -1,10 +1,8 @@
 // routes.tsx
-import { lazy } from "react";
 import { Outlet } from "react-router";
-
-const Page = lazy(() => import("./layouts/Page"));
-const RegisterForm = lazy(() => import("./layouts/register/RegisterForm"));
-const LoginForm = lazy(() => import("./layouts/login/LoginForm"));
+import Page from "./layouts/Page";
+import LoginForm from "./layouts/login/LoginForm";
+import RegisterForm from "./layouts/register/RegisterForm";
 
 export const createRoutes = () => {
   const appRoutes = [
