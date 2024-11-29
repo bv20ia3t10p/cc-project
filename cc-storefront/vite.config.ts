306fadb5e17d4bf342198d091144b4d7b95e68d1
Phuc <beaver20310@gmail.com>
2024-11-29 18:49:05 +0700

    s

--- cc-storefront/vite.config.ts
+++ cc-storefront/vite.config.ts
@@ -1,21 +1,27 @@
 // vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 5173,
+    hmr: true,
     watch: {
       usePolling: true,
     },
   },
+  build: {
+    rollupOptions: {
+      input: "./index.html",
+    },
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   plugins: [react()],
-  base: '/',
+  base: "/",
 });
