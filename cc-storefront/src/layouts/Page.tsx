20e35437b5a11d341c7ff44ca32496482905565d
Phuc <beaver20310@gmail.com>
2024-11-30 19:20:26 +0700

    2

--- cc-storefront/src/layouts/Page.tsx
+++ cc-storefront/src/layouts/Page.tsx
@@ -1,95 +1,19 @@
 import Logo from "@/components/Logo";
 import { IProps } from "@/interfaces/IProps";
 import {
-  GithubOutlined,
-  SearchOutlined,
-  ShoppingCartOutlined,
-  UserOutlined,
+  GithubOutlined
 } from "@ant-design/icons";
-import { Badge, Button, Col, Input, Layout, Row, Typography } from "antd";
-import { Content, Footer, Header } from "antd/es/layout/layout";
+import { Col, Layout, Row, Typography } from "antd";
+import { Content, Footer } from "antd/es/layout/layout";
 import Link from "antd/es/typography/Link";
 import React from "react";
+import { AppHeader } from "./AppHeader";
 
 const Page: React.FC<IProps> = ({ children }) => {
   return (
     <Layout className="min-h-screen">
-      <Header
-        style={{ backgroundColor: "white", display: "flex", height: "10vh" }}
-        className="w-full border-b-2 border-gray-200"
-      >
-        <Row align={"middle"} className="w-full">
-          {/* Icon */}
-          <Col span={2}>
-            <Row align={"middle"}>
-              <Logo />
-            </Row>
-          </Col>
-          {/* Search bar */}
-          <Col span={6}>
-            <Row align={"middle"}>
-              <Input.Search
-                inputMode="search"
-                className="h-full"
-                allowClear
-                placeholder="Search for products, names, brands,... "
-                enterButton={<SearchOutlined />}
-                type="outlined"
-                size="middle"
-                onSearch={() => {}}
-              />
-            </Row>
-          </Col>
-          {/* Other items */}
-          <Col span={16}>
-            <Row justify={"space-evenly"} align={"middle"} className="h-full">
-              <Col
-                span={16}
-                xs={{
-                  span: 14,
-                }}
-                xl={{
-                  span: 4,
-                }}
-                style={{
-                  marginLeft: "6px",
-                }}
-              >
-                <Row justify={"space-evenly"}>
-                  <Link>
-                    <Typography>Home</Typography>
-                  </Link>
-                  <Link>
-                    <Typography>Products</Typography>
-                  </Link>
-                  <Link>
-                    <Typography>About</Typography>
-                  </Link>
-                </Row>
-              </Col>
-              <Col
-                span={0}
-                xl={{
-                  span: 14,
-                }}
-              ></Col>
-              <Col>
-                <Row className="h-full" justify={"space-between"}>
-                  <Col span={8}>
-                    <Badge count={5} overflowCount={99} size="default">
-                      <Button size={"large"} icon={<ShoppingCartOutlined />} />
-                    </Badge>
-                  </Col>
-                  <Col span={8}>
-                    <Button size="large" icon={<UserOutlined />} />
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Header>
-      <Content className="py-12 bg-white">{children}</Content>
+      <AppHeader />
+      <Content className="bg-white ">{children}</Content>
       <Footer>
         <Row>
           <Col span={20}>
@@ -119,3 +43,4 @@ const Page: React.FC<IProps> = ({ children }) => {
 };
 
 export default Page;
+
--- cc-storefront/src/layouts/storefront/StoreFront.tsx
+++ cc-storefront/src/layouts/storefront/StoreFront.tsx
@@ -1,71 +1,26 @@
-import { Button, Col, Row, Typography } from "antd";
+import { Breadcrumb, Button, Col, Row, Typography } from "antd";
 import React from "react";
+import { Banner } from "./Banner";
+import BreadcrumbItem from "antd/es/breadcrumb/BreadcrumbItem";
+import { HomeOutlined } from "@ant-design/icons";
+import { Link } from "react-router";
 
 export const StoreFront: React.FC = () => {
   return (
     <>
-      <div
-        className="w-screen"
-        style={{
-          backgroundColor: '#f0efeb'
-        }}
-      >
-        <Row>
-          <Col span={12}>
-            <Row align={'bottom'} justify={'center'}
-              className="h-1/2">
-              <Typography className="text-[3vw] font-semibold mb-4">
-                Convenience at your doorstep
-              </Typography>
-            </Row>
-            <Row align={'top'} justify={'space-evenly'} className="w-full text-center">
-              <Button type='primary' size='large'>
-                SHOP NOW
-              </Button>
-            </Row>
-          </Col>
-          <Col span={12}>
-            <Row className="z-10 self-end w-full h-full pt-10 md:w-8/12 img-container img-contain md:py-0">
-              <figure className="w-full max-w-none">
-                <picture>
-                  <source
-                    srcSet="https://media.crystallize.com/frntr/22/6/8/1/@100/big-sale-banner.avif 100w, 
-                    https://media.crystallize.com/frntr/22/6/8/1/@200/big-sale-banner.avif 200w, 
-                    https://media.crystallize.com/frntr/22/6/8/1/@500/big-sale-banner.avif 500w, 
-                    https://media.crystallize.com/frntr/22/6/8/1/@768/big-sale-banner.avif 768w"
-                    type="image/avif"
-                    sizes="(max-width: 500px) 300px, 700px"
-                  />
-                  <source
-                    srcSet="https://media.crystallize.com/frntr/22/6/8/1/@100/big-sale-banner.webp 100w, 
-                    https://media.crystallize.com/frntr/22/6/8/1/@200/big-sale-banner.webp 200w, 
-                    https://media.crystallize.com/frntr/22/6/8/1/@500/big-sale-banner.webp 500w, 
-                    https://media.crystallize.com/frntr/22/6/8/1/@768/big-sale-banner.webp 768w"
-                    type="image/webp"
-                    sizes="(max-width: 500px) 300px, 700px"
-                  />
-                  <source
-                    srcSet="https://media.crystallize.com/frntr/22/6/8/1/@100/big-sale-banner.jpeg 100w, 
-                    https://media.crystallize.com/frntr/22/6/8/1/@200/big-sale-banner.jpeg 200w, 
-                    https://media.crystallize.com/frntr/22/6/8/1/@500/big-sale-banner.jpeg 500w, 
-                    https://media.crystallize.com/frntr/22/6/8/1/@768/big-sale-banner.jpeg 768w"
-                    type="image/jpeg"
-                    sizes="(max-width: 500px) 300px, 700px"
-                  />
-                  <img
-                    src="https://media.crystallize.com/frntr/22/6/8/1/big-sale-banner.jpg"
-                    alt="Big sale banner"
-                    width="768"
-                    height="639"
-                    loading="lazy"
-                  />
-                </picture>
-                <figcaption></figcaption>
-              </figure>
-            </Row>
-          </Col>
-        </Row>
-      </div>
+      <Banner />
+      <Breadcrumb className="mx-20 my-4">
+        <BreadcrumbItem>
+          <Link to='/'>
+            <Typography>
+              <HomeOutlined /> Home
+            </Typography>
+          </Link>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      
     </>
   );
 };
+
+
